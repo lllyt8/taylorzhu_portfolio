8cac07234275ebe9b5744a592bc2b47bab461711
Taylor <taylorich868@gmail.com>
2025-04-17 23:10:52 -0700

    change 2

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { splitVendorChunkPlugin } from 'vite'
-import { compression } from 'vite-plugin-compression2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: '.',
   publicDir: 'client/public',
-  plugins: [
-    react(),
-    splitVendorChunkPlugin(),
-    compression({
-      algorithm: 'gzip',
-      exclude: [/\.(br)$/, /\.(gz)$/],
-    }),
-  ],
+  plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'ui-vendor': ['framer-motion'],
-          'markdown': ['react-markdown'],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-    sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
+    emptyOutDir: true
+  }
 })
